77d96cd75b496e0aca7153636dd4f81f717f7f5f
Saish Sankhe <saishsankhe23@gmail.com>
2021-05-14 01:12:52 -0400

    mostly final commit

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, url_for, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import yfinance as yf
-import pandas as pd
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///symbols.db'
