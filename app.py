2c6b55d527521d20cf10a643f796ccaefc97b320
Saish Sankhe <saishsankhe23@gmail.com>
2021-05-13 21:16:51 -0400

    updated profile info, added db

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import yfinance as yf
 import pandas as pd
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite///symbols.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///symbols.db'
 db = SQLAlchemy(app)
 
-class Symbols(db.model):
+class Symbols(db.Model):
     id = db.Column(db.String(10), primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    address = db.Column(db.String(5000), default=None)
+    address = db.Column(db.String(100), default=None)
     city = db.Column(db.String(50), default=None)
     state = db.Column(db.String(50), default=None)
     zip = db.Column(db.Integer, default=None)
@@ -19,15 +19,21 @@ class Symbols(db.model):
     sector = db.Column(db.String(500), default=None)
     industry = db.Column(db.String(500), default=None)
     fullTimeEmployees = db.Column(db.Integer, default=None)
+    description = db.Column(db.String(5000), default=None)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == "POST":
         symbol = request.form['symbol'].lower()
     else:
-        symbol = 'tsla'
+        if request.args:
+            symbol = request.args['symbol'] if request.args['symbol'] else 'tsla'    
+        else:
+            symbol = 'tsla'
 
     data = yf.Ticker(symbol)
+
+    error = addToDb(symbol, data)
 
     return render_template('index.html', data=data.info)
 
@@ -94,8 +100,46 @@ def profile():
     symbol = request.args['symbol']
 
     data = yf.Ticker(symbol)
+    dbData = Symbols.query.get(symbol)
 
-    return render_template('profile.html', data=data.info)
+    return render_template('profile.html', data=data.info, info=dbData)
+
+def addToDb (symbol, data):
+    error = False
+
+    # try:
+    #     toDel = Symbols.query.get(symbol)
+    #     db.session.delete(toDel)
+    #     db.session.commit()
+    # except:
+    #     error=True
+
+    if(db.session.query(db.exists().where(Symbols.id == symbol)).scalar() != True):
+        id = symbol
+        name = data.info['shortName']
+        address = data.info['address1']
+        city = data.info['city']
+        state = data.info['state']
+        zip = data.info['zip']
+        phone = data.info['phone']
+        website = data.info['website']
+        sector = data.info['sector']
+        industry = data.info['industry']
+        fullTimeEmployees = data.info['fullTimeEmployees']
+        description = data.info['longBusinessSummary']
+
+        new_symbol = Symbols(id = id, name = name, address = address, city = city, 
+                            state = state, zip = zip, phone = phone, website = website, 
+                            sector = sector, industry = industry, 
+                            fullTimeEmployees = fullTimeEmployees, description = description)
+        
+        try:
+            db.session.add(new_symbol)
+            db.session.commit()
+        except:
+            error = True
+    
+    return error
 
 if __name__ == "__main__":
     app.run(debug = True)
