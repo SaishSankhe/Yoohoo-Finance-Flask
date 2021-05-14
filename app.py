060f671f490c6dcd20499055434eee7066e953e1
Saish Sankhe <saishsankhe23@gmail.com>
2021-05-14 00:56:10 -0400

    added comments

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///symbols.db'
 db = SQLAlchemy(app)
 
+# define our database model
 class Symbols(db.Model):
     id = db.Column(db.String(10), primary_key=True)
     name = db.Column(db.String(50), nullable=False)
@@ -23,74 +24,138 @@ class Symbols(db.Model):
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    """ This function is for index route ('/').
+        It fetches data from yfinance module for POSTed symbol or 'aapl' by default.
+        It then sends the data to 'index.html' template which renders the data and chart.
+
+    Returns:
+        jinja template
+    """
+
+    # check if the method is POST, i.e. is something is sent from form
     if request.method == "POST":
         symbol = request.form['symbol'].lower()
     else:
+        # this is to check if any symbol is passed as url parameters
         if request.args:
-            symbol = request.args['symbol'] if request.args['symbol'] else 'tsla'    
+            symbol = request.args['symbol'] if request.args['symbol'] else 'aapl'    
         else:
-            symbol = 'tsla'
+            symbol = 'aapl'
 
+    # get the data from yfinance module
     data = yf.Ticker(symbol)
     
+    # if there's no data available for any symbol, it means that there's no such symbol
     if len(data.info) <= 1:
+        # render the error message
         return render_template('index.html', error="No such symbol. Try again.")
     else:
+        '''
+        # add the static data to SQL database and get if there are any errors in adding it
+        # this data will be used for '/profile
+        # '''
         error = addToDb(symbol, data)
 
+        # render the index page and pass the data.info got from the yfinance module
         return render_template('index.html', data=data.info)
 
 @app.route('/history', methods=["POST", "GET"])
 def history():
-    #get the query string parameters
-	symbol = request.args.get('symbol', default="AAPL")
-	period = '2y'
-	interval = '1mo'
+    """ This function is for getting the historical data from yfinace module for any symbol
+        that is passed as a url parameter and then returning the data to javascript file
+        to use this data for creating a chart
 
-	#pull the quote
-	quote = yf.Ticker(symbol)	
-	#use the quote to pull the historical data from Yahoo finance
-	hist = quote.history(period=period, interval=interval)
-    
-	#convert the historical data to JSON
-	data = hist.to_json()
-	#return the JSON in the HTTP response
-	return data
+    Returns:
+        JSON
+    """
+
+    # get the symbol from query string parameters
+    symbol = request.args.get('symbol', default="AAPL")
+    period = '2y' # set default period
+    interval = '1mo' # set default interval
+
+    #pull the quote
+    quote = yf.Ticker(symbol)
+
+    #use the quote to pull the historical data from Yahoo finance
+    hist = quote.history(period=period, interval=interval)
+
+    #convert the historical data to JSON
+    data = hist.to_json()
+
+    #return the JSON in the HTTP response
+    return data
 
 @app.route('/get-quote-data')
 def getQuoteData():
+    """ This function is to get the latest updated quote (every 5 seconds) data from yfinance,
+        return the JSON to javascript, where current market price and difference percent
+        is calculate and updated on the webpage using innerHtml
+
+    Returns:
+        JSON
+    """
+
+    # get the symbol from url parameter
     symbol = request.args['symbol']
 
     data = yf.Ticker(symbol)
 
+    # return JSON data
     return (jsonify(data.info))
 
 @app.route('/holders')
 def holders():
+    """ This function is get the holders data from module and pass it to 'holders.html'
+        It also checks if any symbol has holders data or not.
+        If not, returns False, so that the specific block is not displayed on webpage.
+
+    Returns:
+        jinja template
+    """
     symbol = request.args['symbol']
 
     data = yf.Ticker(symbol)
     
+    # get major holders from data
     major_holders = data.major_holders
+
+    '''
+    # this is to check if it really contains holder data or some other data
+    # after assessing the data for symbols which do not have holders data stored,
+    # this returned a different data like 52DayAvg, etc. Therefore, if we check if there
+    # is a '%' in [0][0] index, we may know if it is holders data or not'''
     if '%' in major_holders[0][0]:
         major_holders.columns = ["shares", "holders"]
 
+        # convert it to a dict
         majorDict = major_holders.to_dict()
         majorHolders = []
 
+        # put the data from dict to an array of dicts
+        # this is to properly pass the data to the template as to read it properly
         for i in range(len(majorDict['shares'])):
             majorHolders.append({
                 "share": majorDict['shares'][i],
                 "holder": majorDict['holders'][i]
             })
     else:
+        # set false to not load the block
         majorHolders = False
 
     inst_holders = data.institutional_holders
+
+    '''
+    # this is to check if it really contains holder data or some other data
+    # after assessing the data for symbols which do not have holders data stored,
+    # this returned data like DailyAvg, etc. Therefore, if we check if the column names
+    # has "Holder" in it, we can be sure if it is holders data'''
     if 'Holder' in inst_holders.columns:
         instDict = inst_holders.to_dict()
         instHolders = []
 
+        # put the data from dict to an array of dicts
+        # this is to properly pass the data to the template as to read it properly
         for i in range(len(instDict['Holder'])):
             instHolders.append({
                 "holder": instDict['Holder'][i],
@@ -100,20 +165,38 @@ def holders():
                 "value": '{:,}'.format(instDict['Value'][i])
             })
     else:
+        # set false to not load the block
         instHolders = False
 
+    # pass the data and render the template
     return render_template('holders.html', data=data.info, major=majorHolders, inst=instHolders)
 
 @app.route('/profile')
 def profile():
+    """ This function is used to get the data from SQL database and pass it to template
+
+    Returns:
+        jinja template
+    """
     symbol = request.args['symbol']
 
     data = yf.Ticker(symbol)
+
+    # get the data from the database
     dbData = Symbols.query.get(symbol)
 
     return render_template('profile.html', data=data.info, info=dbData)
 
 def addToDb (symbol, data):
+    """ This is a helper function to load the data to database
+
+    Args:
+        symbol (string)
+        data (dict)
+
+    Returns:
+        bool: if there was any error while loading the data or not
+    """
     error = False
 
     # try:
@@ -123,6 +206,8 @@ def addToDb (symbol, data):
     # except:
     #     error=True
 
+    # check if there already exists any row with same symbol
+    # if not, create one
     if(db.session.query(db.exists().where(Symbols.id == symbol)).scalar() != True):
         id = symbol
         name = data.info['shortName']
--- static/scripts/site.js
+++ static/scripts/site.js
@@ -1,6 +1,8 @@
 const currentVal = document.getElementById('current-percent').innerHTML;
 const currentEle = document.getElementById('current');
 
+// check the value of current percent
+// set green if above 0 and red if in negative
 if (currentVal < 0) {
 	currentEle.classList.remove('green');
 	currentEle.classList.add('red');
@@ -9,49 +11,63 @@ if (currentVal < 0) {
 	currentEle.classList.add('green');
 }
 
+// get the url pathname to highlight the navigation item
 const url = window.location.pathname;
 
 if (url === '/') {
+	// get the big numbers and convert them to K, M, B or T
 	const marketCap = document.getElementById('market-cap').innerHTML;
 	const recentQuarter = document.getElementById('recent-quarter').innerHTML;
 	const newRecentQuarter = toIntlCurrency(recentQuarter);
 	const newMarketCap = toIntlCurrency(marketCap);
 
+	// set the new numbers
 	document.getElementById('market-cap').innerHTML = newMarketCap;
 	document.getElementById('recent-quarter').innerHTML = newRecentQuarter;
 
+	// remove selected class attribute from all nav-items
 	const navItems = document.getElementsByClassName('nav-item');
 	for (let item of navItems) {
 		item.classList.remove('nav-item-selected');
 	}
 
+	// add selected class to summary nav-item
 	const summaryEle = document.getElementById('summary');
 	summaryEle.classList.add('nav-item-selected');
 } else if (url === '/holders') {
+	// remove selected class attribute from all nav-items
 	const navItems = document.getElementsByClassName('nav-item');
 	for (let item of navItems) {
 		item.classList.remove('nav-item-selected');
 	}
 
+	// add selected class to holders nav-item
 	const holdersEle = document.getElementById('holders');
 	holdersEle.classList.add('nav-item-selected');
 }
 
 if (url === '/profile') {
+	// remove selected class attribute from all nav-items
 	const navItems = document.getElementsByClassName('nav-item');
 	for (let item of navItems) {
 		item.classList.remove('nav-item-selected');
 	}
 
+	// add selected class to profile nav-item
 	const profileEle = document.getElementById('profile');
 	profileEle.classList.add('nav-item-selected');
 }
 
+// get the current market price to calculate new price and set the innerHtml
 let marketPriceEle = document.getElementById('market-price');
 
 function refreshTime() {
+	// this function is to get the latest data from yfinance
+	// and set the new values to webpage
+	// this ensures that the user gets the latest price, without refreshing
 	const symbol = document.getElementById('symbol').innerHTML;
 
+	// get the data from backend by passing the symbol
 	$.ajax({
 		url: '/get-quote-data?symbol=' + symbol,
 		method: 'GET',
@@ -59,19 +75,27 @@ function refreshTime() {
 	}).done(function (data) {
 		let parseData = data;
 		let marketPrice = parseData.regularMarketPrice;
+		// calculate the percentage
 		let percentVal = (
 			(1 -
 				parseData.regularMarketPreviousClose / parseData.regularMarketPrice) *
 			100
 		).toFixed(2);
+
+		// set the values to webpage
 		marketPriceEle.innerHTML = marketPrice;
 		document.getElementById('current-percent').innerHTML = percentVal;
 	});
 }
 
+// update the values every 5 seconds
+// can change this value, I have kept it 5 sec, not to overload the api and backend
 setInterval(refreshTime, 5000);
 
 function toIntlCurrency(labelValue) {
+	// This function converts the big numbers and returns its respective number
+	// in K, M, B or T
+
 	// Twelve Zeroes for Trillions
 	return Math.abs(Number(labelValue)) >= 1.0e12
 		? (Math.abs(Number(labelValue)) / 1.0e12).toFixed(3) + 'T'
@@ -87,6 +111,7 @@ function toIntlCurrency(labelValue) {
 		: Math.abs(Number(labelValue));
 }
 
+// to load chart every time the content is loaded and only when url is '/'
 document.addEventListener('DOMContentLoaded', function () {
 	if (url === '/') {
 		LoadChart();
@@ -94,7 +119,9 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 LoadChart = function () {
-	console.log('in loadchart');
+	// this function gets the data from backend and passes to RenderChart function to render
+	// the chart
+
 	const symbol = document.getElementById('symbol').innerHTML;
 
 	$.ajax({
@@ -107,7 +134,8 @@ LoadChart = function () {
 };
 
 RenderChart = function (data, symbol) {
-	console.log('in renderchart');
+	// this function renders the chart using Charts.js and data passed
+
 	let priceData = [];
 	let dates = [];
 
@@ -121,6 +149,7 @@ RenderChart = function (data, symbol) {
 		}
 	}
 
+	// to change the color of graph depending of value, green if positive, red if negative
 	let color = '#00873C';
 	if (currentVal > 0) {
 		color = '#00873C';
